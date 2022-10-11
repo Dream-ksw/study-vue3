e462499bd3ae413fbc4d126c5e7be976a3ae1d23
Dream-v10 <85750637+Dream-v10@users.noreply.github.com>
2022-10-11 15:31:23 +0800

    项目初始化

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld msg="Study Vue3"/>
 </template>
 
 <script>
@@ -19,8 +18,7 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin: 0;
+  padding: 0;
 }
 </style>
