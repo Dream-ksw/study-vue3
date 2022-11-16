57398dee781c6ad9118d1a5ef7ec759e6b95fc7f
Dream-v10 <85750637+Dream-v10@users.noreply.github.com>
2022-11-16 17:07:34 +0800

    第12节学习中

---
+++ src/components/12-VueCLI和Vite的使用和原理/002_vuecli_demo/src/components/HelloWorld.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <!-- 
+      vite的介绍和发展
+      构建工具: webpack rollup parcel(零配置) gulp(自动化) vite(下一嗲前端开发和构建工具)
+      两部分: 1.开发服务器 2.一套构建命令(rollup)
+     -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
