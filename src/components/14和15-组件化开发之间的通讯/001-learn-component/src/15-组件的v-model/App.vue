1e2cea765eab4bed437242aed06e0c9441755965
kongshiwei <654856052@qq.com>
2022-12-01 23:44:52 +0800

    17节学习完成

---
+++ src/components/14和15-组件化开发之间的通讯/001-learn-component/src/15-组件的v-model/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <!-- <input type="text" v-model="message"> -->
+    <!-- <input type="text" :value="message" @input="message = $event.target.value"> -->
+
+    <!-- 组件上使用v-model -->
+    <!-- v-model在组件上如果默认不加名字的话 传递的其实的是 modelValue -->
+    <!-- <k-input :modelValue="message" @update:modelValue="message = $event"></k-input> -->
+    <!-- <k-input v-model="message"></k-input> -->
+
+    <!-- 组件上可以同时使用多个v-model  可以使用默认的,也可以自定义名称 -->
+    <k-input v-model="message" v-model:title="title"></k-input>
+
+
+
+    <h2>message: {{ message }}</h2>
+    <h2>title: {{ title }}</h2>
+  </div>
+</template>
+
+<script>
+  import KInput from './KInput.vue'
+  export default {
+    components: {
+      KInput
+    },
+    data() {
+      return {
+        message: 'hello world',
+        title: 'abc'
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/14和15-组件化开发之间的通讯/001-learn-component/src/15-组件的v-model/KInput.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <!-- 1.默认绑定和事件处理 -->
+    <!-- <button @click="update">通知父组件改变值</button>
+    <h2>组件通过v-model传递的值: {{ modelValue }}</h2> -->
+
+    <!-- 2.通过input -->
+    <!-- <input type="text" :value="modelValue" @input="update"> -->
+
+    <!-- 3.不能修改父组件传递过来的值  单向数据流 -->
+    <!-- <input type="text" v-model="modelValue"> -->
+
+    <!-- 4.可以使用计算属性 -->
+    <input type="text" v-model="newModelValue">
+    <input type="text" v-model="newTitle">
+  </div>
+</template>
+
+<script>
+  export default {
+    props:{
+      modelValue: String,
+      title: String
+    },
+    computed: {
+      newModelValue: {
+        get() {
+          return this.modelValue
+        },
+        set(value) {
+          this.$emit('update:modelValue', value)
+        }
+      },
+      newTitle: {
+        get() {
+          return this.title
+        },
+        set(value) {
+          this.$emit('update:title', value)
+        }
+      }
+    },
+    methods: {
+      update(e) {
+        this.$emit('update:modelValue', e.target.value)
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
